e38da6e7f2a42136b4bad9646b2f063b249d14f3
josemak25 <amakirij@gmail.com>
2025-03-13 18:11:07 +0100

    feature: add theme mode toggler and news sources

---
+++ app/components/ui/news-sources.tsx
@@ -0,0 +1,50 @@
+import { useLoaderData } from "react-router";
+
+import { cn } from "~/lib/utils";
+import { Button } from "./button";
+import { ScrollArea, ScrollBar } from "./scroll-area";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+
+type NewsSourceProps = {
+  selected?: string;
+  onSelect: (select: string) => void;
+};
+
+export const NewsSources = ({ selected, onSelect }: NewsSourceProps) => {
+  const { sources } = useLoaderData<ArticleLoaderData>();
+
+  return (
+    <ScrollArea
+      aria-orientation="horizontal"
+      className="w-full whitespace-nowrap"
+    >
+      <div className="flex w-max px-4 space-x-4">
+        {sources.map((source) => (
+          <Button
+            variant="ghost"
+            key={source.name}
+            onClick={() => onSelect(source.name)}
+            className={cn(
+              "flex-col h-full cursor-pointer hover:bg-transparent rounded-sm",
+              source.name === selected && "border-b-2"
+            )}
+          >
+            <Avatar
+              className={cn(
+                "size-16 bg-slate-100 flex items-center justify-center p-2",
+                source.name === selected && "bg-red-400"
+              )}
+            >
+              <AvatarImage className="rounded-full" src={source.image} />
+              <AvatarFallback className="uppercase">
+                {source.name.slice(0, 2)}
+              </AvatarFallback>
+            </Avatar>
+            <p className="text-sm font-medium mt-2">{source.name}</p>
+          </Button>
+        ))}
+      </div>
+      <ScrollBar orientation="horizontal" hidden />
+    </ScrollArea>
+  );
+};
